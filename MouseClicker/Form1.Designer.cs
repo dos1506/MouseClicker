328b6d9a88cfd96253e2e888ec251f6db76257f5
Shota Muto <dos9954@gmail.com>
2016-01-23 02:02:13 +0900

    First Commit

---
+++ MouseClicker/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MouseClicker {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.clickDurationBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clickDurationBox
+            // 
+            this.clickDurationBox.Location = new System.Drawing.Point(130, 106);
+            this.clickDurationBox.Name = "clickDurationBox";
+            this.clickDurationBox.Size = new System.Drawing.Size(100, 19);
+            this.clickDurationBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.Location = new System.Drawing.Point(236, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "秒";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "クリック間隔";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ALt+Shift+Aで実行/停止";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clickDurationBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox clickDurationBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MouseClicker/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseClicker {
+    public partial class Form1 : Form {
+        const int MOD_ALT = 0x0001;
+        const int MOD_CONTROL = 0x0002;
+        const int MOD_SHIFT = 0x0004;
+
+        const int WM_HOTKEY = 0x0312;
+
+        const int HOTKEY_ID = 0x0100;
+
+        const int MOUSEEVENTF_LEFTDOWN = 0x2;
+        const int MOUSEEVNETF_LEFTUP = 0x4;
+
+        const bool STOP = false;
+        const bool RUN = true;
+
+        public bool status = STOP;
+
+        public int num = 0;
+
+        // Task中断用
+        public CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+        [DllImport("user32.dll")]
+        extern static int RegisterHotKey(IntPtr HWnd, int id, int mod_key, int key);
+
+        [DllImport("user32.dll")]
+        extern static int UnregisterHotKey(IntPtr HWnd, int id);
+
+        [DllImport("user32.dll")]
+        extern static void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // ホットキーの登録
+            RegisterHotKey(this.Handle, HOTKEY_ID, MOD_ALT | MOD_SHIFT, (int)Keys.A);
+        }
+
+        private void Form1_Closed(object sender, System.EventArgs e) {
+            // ホットキーの登録解除      
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+        }
+
+        protected override void WndProc(ref Message m) {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_HOTKEY && (int)m.WParam == HOTKEY_ID) {
+                if (tokenSource == null)
+                    tokenSource = new CancellationTokenSource();
+
+                var token = tokenSource.Token;
+
+                // ホットキーが押される度に実行/停止を切替え
+                status = !status;
+
+                if (status == RUN) {
+                    Task.Factory.StartNew(() => {
+                        try {
+                            // クリック間隔は秒単位
+                            var clickDuration = double.Parse(clickDurationBox.Text) * 1000;
+
+                            // マウス左クリック
+                            // ホットキーが押されたらループを抜ける
+                            while (!token.IsCancellationRequested) {
+                                System.Threading.Thread.Sleep((int)clickDuration);
+                                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                                mouse_event(MOUSEEVNETF_LEFTUP, 0, 0, 0, 0);
+                            }
+                        }
+                        catch (Exception ex) {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }, token).ContinueWith(t => {
+                        tokenSource.Dispose();
+                        tokenSource = null;
+                        status = STOP;
+                    });
+                }
+                else {
+                    tokenSource.Cancel(true);
+                }
+            }
+        }
+    }
+}
